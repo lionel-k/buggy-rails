d237b30acb0f97213d8822ad827018baeb96d83f
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2019-04-01 11:03:10 +0200

    fixed missing chars

--- spec/search_and_replace_spec.rb
+++ spec/search_and_replace_spec.rb
@@ -3,6 +3,7 @@ require_relative '../snippets/search_and_replace'
 
 describe '#search_and_replace' do
   it 'should return correct results' do
-    expect(search_and_replace("I am going to the market", "market", "mall")).to eq "I am going to the mall"
+    expect(search_and_replace('I am going to the market', 'market', 'mall'))
+      .to eq 'I am going to the mall'
   end
 end
