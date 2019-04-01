d4efd285b595649498ff4260851e5a7e7afcfa01
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2019-04-01 11:01:27 +0200

    fix missing letters

--- snippets/missing_letters.rb
+++ snippets/missing_letters.rb
@@ -1,4 +1,4 @@
 def missing_letters(given_letters)
-  all_letters = given_letters.min.upto(given_letters.min)
-  missing_letters = all_letters - given_letters
+  all_letters = given_letters.min.upto(given_letters.max).to_a
+  all_letters - given_letters
 end
--- spec/missing_letters_spec.rb
+++ spec/missing_letters_spec.rb
@@ -3,6 +3,6 @@ require_relative '../snippets/missing_letters'
 
 describe '#missing_letters' do
   it 'should return correct results' do
-    expect(missing_letters(["a", "b", "f", "h"])).to eq ["c", "d", "e", "g"]
+    expect(missing_letters(['a', 'b', 'f', 'h'])).to eq ['c', 'd', 'e', 'g']
   end
 end
